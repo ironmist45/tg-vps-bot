56bf4b47002f469515cc27f1524a7b7b9fd950f9
ironmist45 <paradero@gmail.com>
2026-04-13 09:31:36 +0000

    Update cli.h

--- include/cli.h
+++ include/cli.h
@@ -1,1 +1,14 @@
+#ifndef CLI_H
+#define CLI_H
 
+typedef struct {
+    char config_path[256];
+    int show_help;
+    int show_version;
+} cli_args_t;
+
+int cli_parse(int argc, char *argv[], cli_args_t *args);
+void cli_print_help();
+void cli_print_version();
+
+#endif
