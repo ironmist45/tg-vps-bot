1aa7dc2579050262a89ab3b89dff7e95c7273108
ironmist45 <paradero@gmail.com>
2026-04-13 10:05:05 +0000

    Update telegram.h

--- include/telegram.h
+++ include/telegram.h
@@ -3,10 +3,15 @@
 
 #include "config.h"
 
+// init / cleanup
 int telegram_init(const config_t *cfg);
 void telegram_cleanup();
 
+// polling
 int telegram_poll();
+
+// отправка сообщений
 int telegram_send_message(const char *text);
+int telegram_send_long_message(const char *text);
 
 #endif
