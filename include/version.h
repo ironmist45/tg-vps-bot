25a79a3d50acfa24e57c23cabfac06fb31497fbb
ironmist45 <paradero@gmail.com>
2026-04-13 12:29:00 +0000

    Update version.h

--- include/version.h
+++ include/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APP_NAME    "tg-bot"
-#define APP_VERSION "1.0.0"
-#define TARGET_OS   "ubuntu 18.04.6"
+#define APP_VERSION "1.2.1"
+#define TARGET_OS   "Ubuntu 18.04.6 LTS x86_64"
 
 // 🔥 новые поля
 #define APP_YEAR    "2026"
