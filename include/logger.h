b1ef00123b43f5bffa3fe0325e78c8bc8994fcf4
ironmist45 <paradero@gmail.com>
2026-04-13 09:53:57 +0000

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -4,13 +4,13 @@
 #include <stdio.h>
 
 typedef enum {
+    LOG_DEBUG = 0,
     LOG_INFO,
     LOG_WARN,
-    LOG_ERROR,
-    LOG_DEBUG
+    LOG_ERROR
 } log_level_t;
 
-int logger_init(const char *log_path);
+int logger_init(const char *path);
 void logger_close();
 
 void log_msg(log_level_t level, const char *fmt, ...);
