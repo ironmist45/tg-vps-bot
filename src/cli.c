35651351daec1942d16dba0d2616754eb94d9bf9
ironmist45 <paradero@gmail.com>
2026-04-13 11:41:52 +0000

    Update cli.c

--- src/cli.c
+++ src/cli.c
@@ -4,49 +4,66 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>   // 🔥 важно
 
 int cli_parse(int argc, char *argv[], cli_args_t *args) {
     memset(args, 0, sizeof(cli_args_t));
 
     int opt;
 
-    // getopt только для коротких
-    while ((opt = getopt(argc, argv, "c:")) != -1) {
+    // 🔥 описание длинных опций
+    static struct option long_options[] = {
+        {"config",  required_argument, 0, 'c'},
+        {"help",    no_argument,       0, 'h'},
+        {"version", no_argument,       0, 'v'},
+        {0, 0, 0, 0}
+    };
+
+    opterr = 0; // отключаем авто-ошибки getopt
+
+    while ((opt = getopt_long(argc, argv, "c:hv", long_options, NULL)) != -1) {
         switch (opt) {
             case 'c':
-                strncpy(args->config_path, optarg, sizeof(args->config_path) - 1);
+                strncpy(args->config_path, optarg,
+                        sizeof(args->config_path) - 1);
                 break;
-            default:
-                return -1;
-        }
-    }
 
-    // обработка длинных аргументов вручную
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--help") == 0) {
-            args->show_help = 1;
-        }
-        else if (strcmp(argv[i], "--version") == 0) {
-            args->show_version = 1;
+            case 'h':
+                args->show_help = 1;
+                break;
+
+            case 'v':
+                args->show_version = 1;
+                break;
+
+            case '?':
+                // неизвестный аргумент
+                return -1;
         }
     }
 
     return 0;
 }
 
+// ===== HELP =====
+
 void cli_print_help() {
     printf("Telegram VPS Bot\n\n");
+
     printf("Usage:\n");
-    printf("  tg_bot -c <config_path>\n\n");
+    printf("  tg-bot [OPTIONS]\n\n");
 
     printf("Options:\n");
-    printf("  -c <path>     Path to config file\n");
-    printf("  --help        Show this help message\n");
-    printf("  --version     Show program version\n\n");
+    printf("  -c, --config <path>   Path to config file\n");
+    printf("  -h, --help            Show this help message\n");
+    printf("  -v, --version         Show program version\n\n");
 
-    printf("Example:\n");
-    printf("  tg_bot -c /etc/tg-bot/config.conf\n");
+    printf("Examples:\n");
+    printf("  tg-bot -c /etc/tg-bot/config.conf\n");
+    printf("  tg-bot --config=/etc/tg-bot/config.conf\n");
 }
+
+// ===== VERSION =====
 
 void cli_print_version() {
     printf("%s version %s\n", APP_NAME, APP_VERSION);
