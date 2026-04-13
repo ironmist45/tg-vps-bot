3318e8e9bd15895bb7c91f0236576cc4988464bc
ironmist45 <paradero@gmail.com>
2026-04-13 13:21:35 +0000

    Update version.h

--- include/version.h
+++ include/version.h
@@ -2,11 +2,11 @@
 #define VERSION_H
 
 #define APP_NAME    "tg-bot"
-#define APP_VERSION "1.2.1"
+#define APP_VERSION "1.2.2"
 #define TARGET_OS   "Ubuntu 18.04.6 LTS x86_64"
 
 // 🔥 новые поля
-#define APP_CODENAME    "Sentinel"
+#define APP_CODENAME    "Nebula"
 #define APP_YEAR    "2026"
 #define APP_AUTHOR  "dedoverde"
 
