0c899c008c389956fc8452501128eb0e7f0765f8
ironmist45 <paradero@gmail.com>
2026-04-13 09:28:12 +0000

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,1 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+#define MAX_TOKEN_LEN 256
+#define MAX_PATH_LEN 256
+
+typedef struct {
+    char token[MAX_TOKEN_LEN];
+    long chat_id;
+    int poll_timeout;
+    char log_file[MAX_PATH_LEN];
+} config_t;
+
+int config_load(const char *path, config_t *cfg);
+
+#endif
