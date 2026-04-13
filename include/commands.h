d1e92df803c71aafde60e478bec73391c445d7f3
ironmist45 <paradero@gmail.com>
2026-04-13 11:14:16 +0000

    Update commands.h

--- include/commands.h
+++ include/commands.h
@@ -3,6 +3,9 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-int commands_handle(const char *text, char *response, size_t resp_size);
+int commands_handle(long chat_id,
+                    const char *text,
+                    char *response,
+                    size_t resp_size);
 
 #endif
