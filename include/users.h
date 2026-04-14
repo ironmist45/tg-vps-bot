7a0ee6b8c0261b51ff20e90c8da3982d5cb247de
ironmist45 <paradero@gmail.com>
2026-04-14 08:33:40 +0000

    Update users.h

--- include/users.h
+++ include/users.h
@@ -1,8 +1,14 @@
-#include <stddef.h>
-
 #ifndef USERS_H
 #define USERS_H
 
+#include <stddef.h>
+
+// ===== LOW LEVEL =====
+// Возвращает список залогиненных пользователей (who)
 int users_get_logged(char *buffer, size_t size);
+
+// ===== HIGH LEVEL (используется в commands.c) =====
+// Красивый форматированный вывод для Telegram
+int users_get(char *buffer, size_t size);
 
 #endif
