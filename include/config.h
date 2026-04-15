7c2ddcc68cc67c9eb020a4e1a883bb5d96333903
ironmist45 <paradero@gmail.com>
2026-04-15 12:25:07 +0000

    Update config.h

--- include/config.h
+++ include/config.h
@@ -3,13 +3,22 @@
 
 #include <stddef.h>
 
+typedef enum {
+    LOG_ERROR = 0,
+    LOG_WARN,
+    LOG_INFO,
+    LOG_DEBUG
+} log_level_t;
+
 typedef struct {
     char token[128];
     long chat_id;
     int poll_timeout;
     char log_file[256];
 
-    int token_ttl;   // ✅ ДОБАВИЛИ
+    int token_ttl;
+
+    log_level_t log_level;   // ✅ LOG_LEVEL
 } config_t;
 
 int config_load(const char *path, config_t *cfg);
