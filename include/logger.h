e1a5765da999a42857c68060659a533bb9b81689
ironmist45 <paradero@gmail.com>
2026-04-15 18:13:18 +0000

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -37,4 +37,7 @@ const char *logger_level_to_string(log_level_t level);
 #define LOG_STATE(level, fmt, ...) \
     log_msg(level, "[STATE] " fmt, ##__VA_ARGS__)
 
+#define LOG_CMD(level, fmt, ...) \
+    log_msg(level, "[CMD] " fmt, ##__VA_ARGS__)
+
 #endif
