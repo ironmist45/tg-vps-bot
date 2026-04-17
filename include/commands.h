146c5c9be8dcafc39d2b573563accd970b368cbb
ironmist45 <paradero@gmail.com>
2026-04-17 09:42:37 +0000

    Update commands.h

--- include/commands.h
+++ include/commands.h
@@ -8,6 +8,20 @@ typedef enum {
     RESP_PLAIN = 1
 } response_type_t;
 
+// 👇 ДОБАВИЛИ
+typedef int (*command_handler_t)(int argc, char *argv[],
+                                long chat_id,
+                                char *response,
+                                size_t resp_size,
+                                response_type_t *resp_type);
+
+typedef struct {
+    const char *name;
+    command_handler_t handler;
+    const char *description;
+} command_t;
+
+// 👇 API
 int commands_handle(const char *text,
                     long chat_id,
                     char *response,
