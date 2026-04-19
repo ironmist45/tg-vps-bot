6618f9f03e2d811b1eb8de42b12dece8d58007a1
ironmist45 <paradero@gmail.com>
2026-04-19 17:43:17 +0000

    Create cmd_security.h

---
+++ include/cmd_security.h
@@ -0,0 +1,12 @@
+#ifndef CMD_SECURITY_H
+#define CMD_SECURITY_H
+
+#include "commands.h"
+
+int cmd_fail2ban(int argc, char *argv[],
+                 long chat_id,
+                 char *response,
+                 size_t resp_size,
+                 response_type_t *resp_type);
+
+#endif
