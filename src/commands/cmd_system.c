1f49328bf78a9701f04e75a1b61a0f5cf9b29f22
ironmist45 <paradero@gmail.com>
2026-04-19 21:10:30 +0000

    Update cmd_system.c
    fix(warnings): silence unused chat_id warnings in command handlers
    
    - Added (void)chat_id where parameter is no longer used
    - Keeps clean -Wall -Wextra build
    - No functional changes

--- src/commands/cmd_system.c
+++ src/commands/cmd_system.c
@@ -46,9 +46,9 @@ int cmd_status(int argc, char *argv[],
                    long chat_id,
                    char *resp, size_t size,
                    response_type_t *resp_type) {
-    (void)argc;
-
-    if (resp_type) *resp_type = RESP_MARKDOWN;
+  
+    (void)argc;      // unused
+    (void)chat_id;  // unused
 
     if (!argv || !argv[0]) {
         snprintf(resp, size, "Invalid command");
@@ -70,8 +70,9 @@ int cmd_status_mini(int argc, char *argv[],
                         long chat_id,
                         char *resp, size_t size,
                         response_type_t *resp_type) {
-
-    (void)argc;
+  
+  (void)argc;      // unused
+  (void)chat_id;  // unused
 
     if (resp_type) *resp_type = RESP_MARKDOWN;
 
