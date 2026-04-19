6f349f9b57f5f2e899a9fcaa0d4b8e5f01dc8f6a
ironmist45 <paradero@gmail.com>
2026-04-19 13:04:50 +0000

    Create cmd_system.h
    feat(commands): add cmd_system.h interface

---
+++ include/cmd_system.h
@@ -0,0 +1,17 @@
+#ifndef CMD_SYSTEM_H
+#define CMD_SYSTEM_H
+
+#include <stddef.h>
+#include "commands.h"
+
+int cmd_about(int argc, char **argv,
+              long chat_id,
+              char *resp, size_t size,
+              response_type_t *resp_type);
+
+int cmd_ping(int argc, char **argv,
+             long chat_id,
+             char *resp, size_t size,
+             response_type_t *resp_type);
+
+#endif
