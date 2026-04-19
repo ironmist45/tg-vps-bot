7cb344b69b411874d30e0132202639d56d04f998
ironmist45 <paradero@gmail.com>
2026-04-19 15:56:53 +0000

    Update commands.c
    refactor(commands): remove cmd_users

--- src/commands.c
+++ src/commands.c
@@ -30,7 +30,6 @@ extern long g_reboot_requested_by;
 
 static int cmd_help(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_logs(int, char **, long, char *, size_t, response_type_t *);
-static int cmd_users(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_fail2ban(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_reboot(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_reboot_confirm(int, char **, long, char *, size_t, response_type_t *);
@@ -99,30 +98,7 @@ static int cmd_logs(int argc, char *argv[],
 }
 
 // ===== USERS =====
-
-static int cmd_users(int argc, char *argv[],
-                     long chat_id,
-                     char *resp, size_t size,
-                     response_type_t *resp_type) {
-    (void)argc;
-
-    if (resp_type) *resp_type = RESP_MARKDOWN;
-
-    if (!argv || !argv[0]) {
-        snprintf(resp, size, "Invalid command");
-        return -1;
-    }
-
-    REQUIRE_ACCESS(chat_id, argv[0], resp, size);
-  
-    if (users_get(resp, size) != 0) {
-        snprintf(resp, size, "⚠️ Failed to get users");
-        return -1;
-    }
-  
-return 0;
-  
-}
+// NOTE: this code has been moved to /src/commands/cmd_services.c!
 
 // ===== FAIL2BAN (🔥 FIXED + LOGGING) =====
 static int cmd_fail2ban(int argc, char *argv[],
