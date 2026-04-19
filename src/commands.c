986037b2f8d09bd6c3d3616ef1d5d7c5fa5c1476
ironmist45 <paradero@gmail.com>
2026-04-19 00:53:50 +0000

    Update commands.c

--- src/commands.c
+++ src/commands.c
@@ -638,14 +638,40 @@ static int cmd_help(int argc, char *argv[],
 
     if (resp_type) *resp_type = RESP_MARKDOWN;
 
-    size_t used = snprintf(resp, size, "*📚 COMMANDS*\n\n");
+    int written = snprintf(resp, size, "*📚 COMMANDS*\n\n");
+
+    if (written < 0 || (size_t)written >= size)
+        return -1;
+
+    size_t used = written;
 
     const char *current_category = NULL;
 
     for (int i = 0; i < commands_count; i++) {
 
+        // 🛡️ защита от underflow
+        if (used >= size - 1)
+            break;
+
         if (!commands[i].category)
             continue;
+
+        // 🔥 новая категория
+        if (!current_category ||
+            strcmp(current_category, commands[i].category) != 0) {
+
+            int written = snprintf(resp + used, size - used,
+                "%s*%s*\n",
+                current_category ? "\n" : "",
+                commands[i].category);
+
+            if (written < 0 || (size_t)written >= size - used)
+                break;
+
+            used += written;
+            current_category = commands[i].category;
+          
+        }
 
         const char *name = commands[i].name;
 
