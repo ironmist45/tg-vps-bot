50192a6f3fc67748b725364ef6b11729c4fec2bd
ironmist45 <paradero@gmail.com>
2026-04-19 15:30:29 +0000

    Update commands.c
    refactor(commands): move cmd_services to cmd_services.c, remove cmd_services from forward declarations, added refactor comments

--- src/commands.c
+++ src/commands.c
@@ -29,7 +29,6 @@ extern long g_reboot_requested_by;
 // ===== forward declarations =====
 
 static int cmd_help(int, char **, long, char *, size_t, response_type_t *);
-static int cmd_services(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_logs(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_users(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_fail2ban(int, char **, long, char *, size_t, response_type_t *);
@@ -37,34 +36,9 @@ static int cmd_reboot(int, char **, long, char *, size_t, response_type_t *);
 static int cmd_reboot_confirm(int, char **, long, char *, size_t, response_type_t *);
 
 // ===== COMMANDS =====
-
-// This code has been moved to /src/commands/cmd_system.c!
-
+// NOTE: this code has been moved to /src/commands/cmd_system.c!
 // ===== SERVICES =====
-
-static int cmd_services(int argc, char *argv[],
-                        long chat_id,
-                        char *resp, size_t size,
-                        response_type_t *resp_type) {
-    (void)argc;
-
-    if (resp_type) *resp_type = RESP_MARKDOWN;
-
-    if (!argv || !argv[0]) {
-        snprintf(resp, size, "Invalid command");
-        return -1;
-    }
-
-    REQUIRE_ACCESS(chat_id, argv[0], resp, size);
-  
-    if (services_get_status(resp, size) != 0) {
-        snprintf(resp, size, "⚠️ Failed to get services");
-        return -1;
-    }
-  
-return 0;
-  
-}
+// NOTE: this code has been moved to /src/commands/cmd_services.c!
 
 // ===== LOGS =====
 
