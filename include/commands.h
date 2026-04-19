77a86a5f4d5316a44b89740106145212936560a1
ironmist45 <paradero@gmail.com>
2026-04-19 18:35:17 +0000

    Update commands.h
    refactor(commands): remove handler declarations from commands.h

--- include/commands.h
+++ include/commands.h
@@ -8,7 +8,6 @@ typedef enum {
     RESP_PLAIN = 1
 } response_type_t;
 
-// 👇 ДОБАВИЛИ
 typedef int (*command_handler_t)(int argc, char *argv[],
                                 long chat_id,
                                 char *response,
@@ -28,70 +27,5 @@ int commands_handle(const char *text,
                     char *response,
                     size_t resp_size,
                     response_type_t *resp_type);
-
-// 👇 Command handlers (exported)
-// General → cmd_system.h
-int cmd_start(int argc, char *argv[],
-              long chat_id,
-              char *response,
-              size_t resp_size,
-              response_type_t *resp_type);
-
-// System info → cmd_system.h
-int cmd_status(int argc, char *argv[],
-               long chat_id,
-               char *response,
-               size_t resp_size,
-               response_type_t *resp_type);
-
-int cmd_status_mini(int argc, char *argv[],
-                    long chat_id,
-                    char *response,
-                    size_t resp_size,
-                    response_type_t *resp_type);
-
-int cmd_about(int argc, char *argv[],
-              long chat_id,
-              char *response,
-              size_t resp_size,
-              response_type_t *resp_type);
-
-int cmd_ping(int argc, char *argv[],
-             long chat_id,
-             char *response,
-             size_t resp_size,
-             response_type_t *resp_type);
-
-// Services → cmd_services.h (создать, отсутствует)
-int cmd_services(int argc, char *argv[],
-                 long chat_id,
-                 char *response,
-                 size_t resp_size,
-                 response_type_t *resp_type);
-
-int cmd_users(int argc, char *argv[],
-              long chat_id,
-              char *response,
-              size_t resp_size,
-              response_type_t *resp_type);
-
-int cmd_logs(int argc, char *argv[],
-             long chat_id,
-             char *response,
-             size_t resp_size,
-             response_type_t *resp_type);
-
-// Control
-int cmd_reboot(int argc, char *argv[],
-               long chat_id,
-               char *response,
-               size_t resp_size,
-               response_type_t *resp_type);
-
-int cmd_reboot_confirm(int argc, char *argv[],
-                       long chat_id,
-                       char *response,
-                       size_t resp_size,
-                       response_type_t *resp_type);
 
 #endif
