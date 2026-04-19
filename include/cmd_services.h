62405a3fd3a2759b81a2edb9ee19e642b14bb8a5
ironmist45 <paradero@gmail.com>
2026-04-19 18:39:25 +0000

    Create cmd_services.h
    feat(commands): add cmd_services.h header

---
+++ include/cmd_services.h
@@ -0,0 +1,27 @@
+#ifndef CMD_SERVICES_H
+#define CMD_SERVICES_H
+
+#include <stddef.h>
+#include "commands.h"
+
+// ===== 👇 Command handlers - Services commands =====
+
+int cmd_services(int argc, char *argv[],
+                 long chat_id,
+                 char *response,
+                 size_t resp_size,
+                 response_type_t *resp_type);
+
+int cmd_users(int argc, char *argv[],
+              long chat_id,
+              char *response,
+              size_t resp_size,
+              response_type_t *resp_type);
+
+int cmd_logs(int argc, char *argv[],
+             long chat_id,
+             char *response,
+             size_t resp_size,
+             response_type_t *resp_type);
+
+#endif
