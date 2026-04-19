6a258416ed849647666b2cbb483d182a4877ed0b
ironmist45 <paradero@gmail.com>
2026-04-19 18:52:02 +0000

    Update cmd_system.h

--- include/cmd_system.h
+++ include/cmd_system.h
@@ -5,7 +5,27 @@
 #include "commands.h"
 
 
-// ==== 👇 Command handlers - /about and /ping commands ====
+// ==== 👇 Command handlers - General & System info ====
+// Bot commands: /start, /status, /status_mini,
+// ============  /about and /ping
+
+int cmd_start(int argc, char *argv[],
+              long chat_id,
+              char *response,
+              size_t resp_size,
+              response_type_t *resp_type);
+
+int cmd_status(int argc, char *argv[],
+               long chat_id,
+               char *response,
+               size_t resp_size,
+               response_type_t *resp_type);
+
+int cmd_status_mini(int argc, char *argv[],
+                    long chat_id,
+                    char *response,
+                    size_t resp_size,
+                    response_type_t *resp_type);
 
 int cmd_about(int argc, char **argv,
               long chat_id,
