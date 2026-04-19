5ec32821625667ff96954b989d99fa040d825e5d
ironmist45 <paradero@gmail.com>
2026-04-19 18:47:06 +0000

    Update cmd_system.h

--- include/cmd_system.h
+++ include/cmd_system.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include "commands.h"
 
+
+// ==== 👇 Command handlers - /about and /ping commands ====
+
 int cmd_about(int argc, char **argv,
               long chat_id,
               char *resp, size_t size,
