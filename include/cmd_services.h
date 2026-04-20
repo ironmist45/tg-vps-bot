3e4cdaad7c4ffb6249976946ce70895fc4fc730c
ironmist45 <paradero@gmail.com>
2026-04-20 00:09:10 +0000

    Update cmd_services.h
    cmd_services: remove legacy handlers for services module
    
    - delete cmd_services, cmd_users, cmd_logs
    - fully migrate services module to handler_v2
    - remove obsolete argc/argv-based handlers

--- include/cmd_services.h
+++ include/cmd_services.h
@@ -10,24 +10,4 @@ int cmd_services_v2(command_ctx_t *ctx);
 int cmd_users_v2(command_ctx_t *ctx);
 int cmd_logs_v2(command_ctx_t *ctx);
 
-// ===== 👇 Commands handlers Legacy =====
-
-int cmd_services(int argc, char *argv[],
-                 long chat_id,
-                 char *response,
-                 size_t resp_size,
-                 response_type_t *resp_type);
-
-int cmd_users(int argc, char *argv[],
-              long chat_id,
-              char *response,
-              size_t resp_size,
-              response_type_t *resp_type);
-
-int cmd_logs(int argc, char *argv[],
-             long chat_id,
-             char *response,
-             size_t resp_size,
-             response_type_t *resp_type);
-
 #endif
