8201084acad35798fcaa76a9004942243a879982
ironmist45 <paradero@gmail.com>
2026-04-20 05:01:59 +0000

    Update cmd_system.c
    (cleanup) remove cmd_ping legacy

--- src/commands/cmd_system.c
+++ src/commands/cmd_system.c
@@ -123,47 +123,6 @@ int cmd_about(int argc, char *argv[],
 
 // ===== System info: /ping command =====
 
-int cmd_ping(int argc, char *argv[],
-             long chat_id,
-             char *resp, size_t size,
-             response_type_t *resp_type) {
-
-    (void)argc; (void)argv; (void)chat_id;
-
-    if (resp_type) *resp_type = RESP_MARKDOWN;
-
-    struct timespec start, end;
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    clock_gettime(CLOCK_MONOTONIC, &end);
-
-    long latency_ms =
-        (end.tv_sec - start.tv_sec) * 1000 +
-        (end.tv_nsec - start.tv_nsec) / 1000000;
-
-    time_t now = time(NULL);
-    long uptime = now - g_start_time;
-
-    int mins = (uptime % 3600) / 60;
-    int secs = uptime % 60;
-
-    LOG_STATE(LOG_INFO,
-        "ping: latency=%ld ms uptime=%dm%ds",
-        latency_ms, mins, secs);
-
-    int written = snprintf(resp, size,
-        "*🏓 PING*\n\n"
-        "Status: `OK`\n"
-        "Latency: `%ld ms`\n"
-        "Uptime: `%dm %ds`",
-        latency_ms, mins, secs);
-
-    if (written < 0 || (size_t)written >= size)
-        return -1;
-
-    return 0;
-}
-
 int cmd_ping_v2(command_ctx_t *ctx)
 {
     struct timespec start, end;
