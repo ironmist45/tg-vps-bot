c6adfcf9129407b1f9ed01ff1db9bf17b8239726
ironmist45 <paradero@gmail.com>
2026-04-23 07:18:12 +0000

    Update telegram_http.c

--- src/telegram_http.c
+++ src/telegram_http.c
@@ -6,10 +6,14 @@
 
 #include "telegram_http.h"
 #include "logger.h"
+#include "lifecycle.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <curl/curl.h>
 
 #define URL_MAX 1024
@@ -64,55 +68,126 @@ int telegram_http_request(const char *method, const char *post_fields, int need_
                           char **out_data, size_t *out_size) {
     LOG_NET(LOG_INFO, "telegram_http_request: ENTRY, method=%s", method);
     
-    CURL *curl = curl_easy_init();
-    LOG_NET(LOG_INFO, "telegram_http_request: after curl_easy_init, curl=%p", (void*)curl);
-    
-    if (!curl) {
-        LOG_NET(LOG_ERROR, "curl_easy_init failed");
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        LOG_NET(LOG_ERROR, "pipe failed");
         return -1;
     }
-
-    setup_curl(curl);
-
-    char url[URL_MAX];
-    snprintf(url, sizeof(url), "%s/%s", g_base_url, method);
-    LOG_NET(LOG_INFO, "telegram_http_request: url=%.100s", url);
-
-    struct memory chunk;
-    chunk.data = NULL;
-    chunk.size = 0;
     
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chunk);
-
-    LOG_NET(LOG_DEBUG, "telegram_http_request: calling curl_easy_perform");
-    CURLcode res = curl_easy_perform(curl);
-    LOG_NET(LOG_DEBUG, "telegram_http_request: curl_easy_perform done, res=%d", res);
+    pid_t pid = fork();
+    if (pid == -1) {
+        LOG_NET(LOG_ERROR, "fork failed");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return -1;
+    }
+    
+    if (pid == 0) {
+        // Дочерний процесс
+        close(pipefd[0]);
+        
+        CURL *curl = curl_easy_init();
+        if (!curl) _exit(1);
+        
+        setup_curl(curl);
+        
+        char url[URL_MAX];
+        snprintf(url, sizeof(url), "%s/%s", g_base_url, method);
+        
+        struct memory chunk = {0};
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chunk);
+        
+        CURLcode res = curl_easy_perform(curl);
+        
+        FILE *pipe_write = fdopen(pipefd[1], "w");
+        if (pipe_write) {
+            fprintf(pipe_write, "%d\n", res);
+            if (chunk.data && need_response) {
+                fprintf(pipe_write, "%zu\n", chunk.size);
+                fwrite(chunk.data, 1, chunk.size, pipe_write);
+            } else {
+                fprintf(pipe_write, "0\n");
+            }
+            fclose(pipe_write);
+        }
+        
+        curl_easy_cleanup(curl);
+        if (chunk.data) free(chunk.data);
+        _exit(0);
+    }
+    
+    // Родительский процесс
+    close(pipefd[1]);
+    
+    CURLcode res = CURLE_OK;
+    char *data = NULL;
+    size_t data_len = 0;
+    
+    time_t start = time(NULL);
+    while (1) {
+        if (lifecycle_shutdown_requested()) {
+            kill(pid, SIGKILL);
+            waitpid(pid, NULL, 0);
+            close(pipefd[0]);
+            return -1;
+        }
+        
+        int status;
+        pid_t result = waitpid(pid, &status, WNOHANG);
+        if (result == pid) {
+            FILE *pipe_read = fdopen(pipefd[0], "r");
+            if (pipe_read) {
+                int res_code;
+                size_t len;
+                if (fscanf(pipe_read, "%d\n%zu\n", &res_code, &len) == 2) {
+                    res = (CURLcode)res_code;
+                    if (len > 0 && need_response) {
+                        data = malloc(len + 1);
+                        if (data) {
+                            fread(data, 1, len, pipe_read);
+                            data[len] = '\0';
+                            data_len = len;
+                        }
+                    }
+                }
+                fclose(pipe_read);
+            }
+            break;
+        }
+        
+        if (time(NULL) - start > 35) {
+            LOG_NET(LOG_ERROR, "HTTP request timed out");
+            kill(pid, SIGKILL);
+            waitpid(pid, NULL, 0);
+            close(pipefd[0]);
+            return -1;
+        }
+        
+        usleep(100000);
+    }
+    
+    close(pipefd[0]);
     
     if (res != CURLE_OK) {
         LOG_NET(LOG_ERROR, "curl error on %s: %s", method, curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-        if (chunk.data) free(chunk.data);
+        if (data) free(data);
         return -1;
     }
-
+    
     if (need_response) {
-        *out_data = chunk.data;
-        *out_size = chunk.size;
-        LOG_NET(LOG_DEBUG, "telegram_http_request: returning data, size=%zu", chunk.size);
+        *out_data = data;
+        *out_size = data_len;
+        LOG_NET(LOG_DEBUG, "telegram_http_request: returning data, size=%zu", data_len);
     } else {
-        LOG_NET(LOG_DEBUG, "telegram_http_request: discarding data, size=%zu", chunk.size);
-        if (chunk.data) {
-            free(chunk.data);
-            chunk.data = NULL;
-        }
+        LOG_NET(LOG_DEBUG, "telegram_http_request: discarding data, size=%zu", data_len);
+        if (data) free(data);
         *out_data = NULL;
         *out_size = 0;
     }
-
-    curl_easy_cleanup(curl);
+    
     LOG_NET(LOG_DEBUG, "telegram_http_request: success");
     return 0;
 }
