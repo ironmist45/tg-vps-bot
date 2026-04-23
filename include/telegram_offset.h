93346d8c936041023283f7bf891705b3d3345d60
ironmist45 <paradero@gmail.com>
2026-04-23 00:50:11 +0000

    Create telegram_offset.h

---
+++ include/telegram_offset.h
@@ -0,0 +1,28 @@
+/**
+ * tg-bot - Telegram bot for system administration
+ * telegram_offset.h - Offset persistence API
+ * MIT License - Copyright (c) 2026
+ */
+
+#ifndef TELEGRAM_OFFSET_H
+#define TELEGRAM_OFFSET_H
+
+/**
+ * Load last processed update_id from disk
+ * @return Last processed update_id, or 0 if no saved state
+ */
+long telegram_offset_load(void);
+
+/**
+ * Save current update_id to disk (atomic via rename)
+ * @param offset Update_id to save
+ */
+void telegram_offset_save(long offset);
+
+/**
+ * Get last saved offset (for shutdown logging)
+ * @return Last offset value
+ */
+long telegram_offset_get_last(void);
+
+#endif // TELEGRAM_OFFSET_H
