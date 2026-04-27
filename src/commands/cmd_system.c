439c15ce8188b5b25068b51cbc5fada3575cfce4
ironmist45 <paradero@gmail.com>
2026-04-27 14:27:20 +0000

    Update cmd_system.c
    fix: use %.14s for exact libcurl version truncation

--- src/commands/cmd_system.c
+++ src/commands/cmd_system.c
@@ -174,7 +174,7 @@ int cmd_about_v2(command_ctx_t *ctx)
         "Uptime: %dd %dh %dm\n"
         "Commit: %s\n"
         "Built: %s\n"
-        "libcurl: %.15s\n"
+        "libcurl: %.14s\n"
         "OpenSSL: %s\n"
         "cJSON: %d.%d.%d",
         APP_NAME, APP_VERSION, APP_CODENAME,
