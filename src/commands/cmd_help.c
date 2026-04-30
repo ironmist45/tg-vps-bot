ca81ef0fbf4c5f6abe8e426a5b59381a5117e9a4
ironmist45 <paradero@gmail.com>
2026-04-30 15:45:17 +0000

    Update cmd_help.c
    fix: use unquoted token in METRICS_CMD(help)

--- src/commands/cmd_help.c
+++ src/commands/cmd_help.c
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * cmd_help.c - /help command handler (V2)
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #include "cmd_help.h"
@@ -84,7 +84,7 @@ int cmd_help_v2(command_ctx_t *ctx)
     }
 
     LOG_CMD_CTX(ctx, LOG_INFO, "help: displayed %zu bytes", used);
-    METRICS_CMD("help");
+    METRICS_CMD(help);
     
     // ✅ Теперь можно использовать reply_markdown
     return reply_markdown(ctx, buffer);
