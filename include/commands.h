f13f7b8edf2204bafbea1ca056474ec45730fb14
ironmist45 <paradero@gmail.com>
2026-05-02 09:57:53 +0000

    Update commands.h
    docs(commands): add MIT license header

--- include/commands.h
+++ include/commands.h
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * commands.h - Command dispatcher types and API
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #ifndef COMMANDS_H
