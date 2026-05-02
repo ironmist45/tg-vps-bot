108b5017d8d7195deb008894786ea010ae47b5b2
ironmist45 <paradero@gmail.com>
2026-05-02 10:01:01 +0000

    Update telegram_offset.h
    docs(telegram_offset): add MIT license header

--- include/telegram_offset.h
+++ include/telegram_offset.h
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * telegram_offset.h - Offset persistence API
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #ifndef TELEGRAM_OFFSET_H
