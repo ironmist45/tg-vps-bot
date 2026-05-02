1dfb467c55fd06978779cb9e548e9372edaef7a0
ironmist45 <paradero@gmail.com>
2026-05-02 09:56:48 +0000

    Update cmd_security.h
    docs(cmd_security): add MIT license header

--- include/cmd_security.h
+++ include/cmd_security.h
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * cmd_security.h - Security command handlers (/fail2ban)
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #ifndef CMD_SECURITY_H
