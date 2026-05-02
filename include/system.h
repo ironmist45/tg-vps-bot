ee14ea546eb6129b1cb8448e7922c861d38b81ff
ironmist45 <paradero@gmail.com>
2026-05-02 10:00:17 +0000

    Update system.h
    docs(system): add MIT license header

--- include/system.h
+++ include/system.h
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * system.h - System information gathering (/status, /health, /ping)
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #ifndef SYSTEM_H
