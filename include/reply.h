af38bf08b8abd5a4ed4eadf38de1e24678b40b00
ironmist45 <paradero@gmail.com>
2026-05-02 09:59:25 +0000

    Update reply.h
    docs(reply): add MIT license header

--- include/reply.h
+++ include/reply.h
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * reply.h - Unified response formatting for command handlers
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #ifndef REPLY_H
