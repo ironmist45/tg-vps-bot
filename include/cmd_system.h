dc24572698fda716a0c9d65bfb597c648e4cb087
ironmist45 <paradero@gmail.com>
2026-05-02 09:57:32 +0000

    Update cmd_system.h
    docs(cmd_system): add MIT license header

--- include/cmd_system.h
+++ include/cmd_system.h
@@ -1,7 +1,7 @@
 /**
  * tg-bot - Telegram bot for system administration
  * cmd_system.h - System information command handlers (/start, /status, /health, /about, /ping)
- * MIT License - Copyright (c) 2026
+ * MIT License - Copyright (c) 2026 ironmist45
  */
 
 #ifndef CMD_SYSTEM_H
